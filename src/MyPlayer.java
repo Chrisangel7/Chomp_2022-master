ed1246af4025dbfd88e8d4b36734280068729109
Chris <Chrisangel_Gonzalez24@milton.edu>
2022-10-28 11:27:03 -0400

    Have printed all resulting boards

--- src/MyPlayer.java
+++ src/MyPlayer.java
@@ -7,8 +7,6 @@ public class MyPlayer {
     public MyPlayer() {
         columns = new int[10];
         make3x3();
-        //ResultBoard();
-
 
         /***
          * This code will run just once, when the game opens.
@@ -18,7 +16,7 @@ public class MyPlayer {
     public void make3x3() {
         for (int i =1; i < 4; i++) {
             for(int o = 0; o<4; o++) {
-                for (int p=0;p<4;p++){
+                for (int p=0;p<4;p++){;
                     if(i>=o && o>=p){
                         System.out.println(i+" "+o+" " +p);
                         ResultBoard(i,o,p);
@@ -28,12 +26,35 @@ public class MyPlayer {
         }
     }
 
-   public void ResultBoard(int i, int o, int p){
-      for (int q=1; q<3; q++){
-           System.out.println(i+" "+o+" "+(p-q));
-      }
-    }
+    public void ResultBoard(int i, int o, int p) {
+        int p1=p;
+        int o1=o;
+        int i1=i;
+        for (int p2 = p-1 ; p2 >= 0; p2--) { // third column
+            System.out.println("Result Board is " + i + " " + o + " " + p2);
+            //System.out.println("Move to get there: " + );
+        }
+        for (int o2 = o-1; o2 >= 0; o2--) { // second column
+            if(o2<p){
+               p=o2;
+            }
+            System.out.println("Result Board is " + i + " " + o2 + " " +p);
+            //p=p1;
+        }
+        p=p1;
+        for (int i2 = i-1; i2 >= 0; i2--) { // first column
+            if(i2<o){
+                o=i2;
+            }
+            if(i2<p){
+                p=i2;
+            }
+            System.out.println("Result Board is " + i2 + " " + o + " " +p);
+           // o=o1;
+        }
 
+        System.out.println();
+    }
     public Point move(Chip[][] pBoard) {
 
         System.out.println("MyPlayer Move");
